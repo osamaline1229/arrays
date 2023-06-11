b4fddd77a1c346d6e6eaf725accd95c691f49453
Osama&line1229 <TRA@DESKTOP-3RC6L19>
2023-06-10 23:40:07 -0700

    update

--- Program.cs
+++ Program.cs
@@ -1,41 +1,10 @@
-﻿namespace recursion
+﻿internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        private static void Main(string[] args)
-        {
-            System.Console.WriteLine(power(3, 3));
-        }
-
-        static int calculateFib(int n)
-        {
-            int secondLastTerm = 0;
-            int lastTerm = 1;
-            int nextTerm = 0;
-
-            for (int i = 2; i <= n; i++)
-            {
-                nextTerm = lastTerm + secondLastTerm;
-                secondLastTerm = lastTerm;
-                lastTerm = nextTerm;
-            }
-            return nextTerm;
-        }
-
-        static int power(int num, int pow)
-        {
-            if (pow == 0)
-            {
-                return 1;
-            }
-            if (pow == 1)
-            {
-                return num;
-            }
-            return num * power(num, pow - 1);
-
-        }
+        int[] a = new int[5];
+        int[] a1 = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(a1[i]);
+        //Read and Write the Arrays
     }
-
 }
-
